e90c3ab995e2e12613a122a70dc3ced4c1c26eb3
wonik-choi <wonik.choi@susimdal.com>
2024-02-16 10:16:51 +0900

    feat: signup -> popup 설정

--- common-accounts-mobile-test/src/components/templates/commonAccountsSignup/CommonAccountsSignup.tsx
+++ common-accounts-mobile-test/src/components/templates/commonAccountsSignup/CommonAccountsSignup.tsx
@@ -1,9 +1,51 @@
 import styles from './CommonAccountsSignup.module.css';
 
 import BaseButton from '../../atoms/button/BaseButton';
+import { RED_SERVICE_ID, BLUE_SERVICE_ID } from '../resource/index';
 
 const CommonAccountsSignup = () => {
-  const openSignupPopup = () => {};
+  let commonAccountsSignupPopup: Window | null = null;
+
+  const setCommonAccountsSignupUrl = (serviceId: string) => {
+    let baseUrl = new URL(`http://dev.accounts.susimdal.com/sign-up`);
+
+    baseUrl.searchParams.set('step', '0');
+    baseUrl.searchParams.set('serviceId', serviceId);
+
+    return baseUrl;
+  };
+
+  const popupfullSizeParams = () => {
+    let params = `width= ` + screen.width;
+    params += `, height= ` + screen.height;
+    params += `, top=0, left=0`;
+    params += `, fullscreen=yes`;
+
+    return params;
+  };
+
+  const popupMessageListener = (event: MessageEvent) => {
+    if (event.origin !== 'http://dev.accounts.susimdal.com') {
+      return;
+    }
+
+    if (event.data === 'popup-close') {
+      commonAccountsSignupPopup?.close();
+      window.removeEventListener('message', popupMessageListener);
+      return;
+    }
+
+    const parsedUserDto = JSON.parse(event.data);
+    console.log(parsedUserDto);
+  };
+
+  const openSignupPopup = () => {
+    const popupParams = popupfullSizeParams();
+    const commonAccountsSignupUrl = setCommonAccountsSignupUrl(BLUE_SERVICE_ID); // blue
+
+    commonAccountsSignupPopup = window.open(commonAccountsSignupUrl, 'blueSignup', popupParams);
+    window.addEventListener('message', popupMessageListener);
+  };
 
   return (
     <>
---
+++ common-accounts-mobile-test/src/components/templates/resource/index.ts
@@ -0,0 +1,2 @@
+export const RED_SERVICE_ID = '65b9a45397ac0837a4e7f661' as const;
+export const BLUE_SERVICE_ID = '65b9a45397ac0837a4e7f662' as const;
